0c23a204bb7368ff54c544ebe46ef7be62769417
Ruan-Pablo <ruestacio@gmail.com>
2024-02-09 15:44:06 -0300

    add botão esc para sair de sugestões

--- app.py
+++ app.py
@@ -13,31 +13,37 @@ for linha in sheet_produtos.iter_rows(min_row=2):
     pyperclip.copy(nome_prod)
     pyautogui.click(1347,317,duration=1)
     pyautogui.hotkey('ctrl','v')
+    pyautogui.hotkey('esc')
 
     descricao = linha[1].value
     pyperclip.copy(descricao)
     pyautogui.click(1241,435, duration=1)
     pyautogui.hotkey('ctrl','v')
+    pyautogui.hotkey('esc')
     
     categoria = linha[2].value
     pyperclip.copy(categoria)
     pyautogui.click(1182,595, duration=1)
     pyautogui.hotkey('ctrl','v')
+    pyautogui.hotkey('esc')
 
     cod_prod = linha[3].value
     pyperclip.copy(cod_prod)
     pyautogui.click(1215,690, duration=1)
     pyautogui.hotkey('ctrl','v')
+    pyautogui.hotkey('esc')
 
     peso = linha[4].value
     pyperclip.copy(peso)
     pyautogui.click(1231,802, duration=1)
     pyautogui.hotkey('ctrl','v')
+    pyautogui.hotkey('esc')
 
     dimensoes = linha[5].value
     pyperclip.copy(dimensoes)
     pyautogui.click(1252,902, duration=1)
     pyautogui.hotkey('ctrl','v')
+    pyautogui.hotkey('esc')
 
     # PRÓXIMA PÁGINA
     pyautogui.click(1173,980, duration=1)
@@ -47,20 +53,24 @@ for linha in sheet_produtos.iter_rows(min_row=2):
     pyperclip.copy(preco)
     pyautogui.click(1261,339, duration=1)
     pyautogui.hotkey('ctrl','v')
+    pyautogui.hotkey('esc')
 
     qtd = linha[7].value
     pyperclip.copy(qtd)
     pyautogui.click(1186,446, duration=1)
     pyautogui.hotkey('ctrl','v')
+    pyautogui.hotkey('esc')
     
     validade = linha[8].value
     pyperclip.copy(validade)
     pyautogui.click(1260,561, duration=1)
     pyautogui.hotkey('ctrl','v')
+    pyautogui.hotkey('esc')
     cor = linha[9].value
     pyperclip.copy(cor)
     pyautogui.click(1262,670, duration=1)
     pyautogui.hotkey('ctrl','v')
+    pyautogui.hotkey('esc')
 
     tamanho  = linha[10].value
     pyautogui.click(1297,788,duration=1)
@@ -74,6 +84,7 @@ for linha in sheet_produtos.iter_rows(min_row=2):
     pyperclip.copy(material)
     pyautogui.click(1297,880, duration=1)
     pyautogui.hotkey('ctrl','v')
+    pyautogui.hotkey('esc')
     
     # proxima página
     pyautogui.click(1172,953,duration=1)
@@ -83,22 +94,27 @@ for linha in sheet_produtos.iter_rows(min_row=2):
     pyperclip.copy(fabricante)
     pyautogui.click(1210,364, duration=1)
     pyautogui.hotkey('ctrl','v')
+    pyautogui.hotkey('esc')
     pais_origem = linha[13].value
     pyperclip.copy(pais_origem)
     pyautogui.click(1211,471, duration=1)
     pyautogui.hotkey('ctrl','v')
+    pyautogui.hotkey('esc')
     obs = linha[14].value
     pyperclip.copy(obs)
     pyautogui.click(1216,601, duration=1)
     pyautogui.hotkey('ctrl','v')
+    pyautogui.hotkey('esc')
     cod_barras = linha[15].value
     pyperclip.copy(cod_barras)
     pyautogui.click(1211,743, duration=1)
     pyautogui.hotkey('ctrl','v')
+    pyautogui.hotkey('esc')
     local_armazem = linha[16].value
     pyperclip.copy(local_armazem)
     pyautogui.click(1215,862, duration=1)
     pyautogui.hotkey('ctrl','v')
+    pyautogui.hotkey('esc')
 
     # concluir
     pyautogui.click(1190,927, duration=1)
